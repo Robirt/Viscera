0b40f583f37e9d274ef4b8f63c47152482d1c7e6
Robert Nancka <robirtpl@outlook.com>
2023-11-25 12:08:18 +0100

    Add skeleton of DA.

---
+++ Viscera.DA/VisceraContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Viscera.DA;
+
+using Microsoft.EntityFrameworkCore;
+
+public class VisceraContext : DbContext
+{
+    public VisceraContext(DbContextOptions<VisceraContext> options) : base(options)
+    {
+
+    }
+}
